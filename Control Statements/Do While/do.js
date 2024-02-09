94d9ecb53dfb6d48123db5aa0cad05abbe7dcd65
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2024-02-09 21:50:48 +0530

    Added JavaScript Files

---
+++ Control Statements/Do While/do.js
@@ -0,0 +1,20 @@
+let i = 0;
+
+do {
+    console.log("In loop alpha:", i);
+    i++;
+} while (i < 5);
+
+let j = 4;
+
+do {
+    console.log("In loop beta:", j);
+    j++;
+} while (j < 5);
+
+let k = 5;
+
+do {
+    console.log("In loop gamma:", k);
+    k++;
+} while (k < 5);
---
+++ Control Statements/Switch/switch.js
@@ -0,0 +1,25 @@
+const grade = "C";
+
+switch (grade) {
+    case "A":
+        console.log("You have got 90+ marks");
+        break;
+    case "B":
+        console.log("You have got 80+ marks");
+        break;
+    case "C":
+        console.log("You have got 70+ marks");
+        break;
+    case "D":
+        console.log("You have got 60+ marks");
+        break;
+    case "E":
+        console.log("You have got 50+ marks");
+        break;
+    case "F":
+        console.log("You have less than 50 marks");
+        break;
+    default:
+        console.log("Not a valid grade");
+        break;
+}
